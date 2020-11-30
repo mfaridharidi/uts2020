682b7a6c18a0c53efed8927e0e3b2ead79d91b39
mfaridharidi <paridmuhammad65@gmail.com>
2020-12-01 03:28:23 +0700

    Menu Rumah Makan

---
+++ app/src/main/java/com/farid/menurumahmakan/makanan/Minuman.java
@@ -0,0 +1,10 @@
+package com.farid.menurumahmakan.makanan;
+
+public class Minuman extends Menu {
+
+    public Minuman(String Makanan, String harga, String deskripsi, int drawableRes) {
+        super("Minuman",Makanan,harga,deskripsi,drawableRes);
+
+    }
+
+}
